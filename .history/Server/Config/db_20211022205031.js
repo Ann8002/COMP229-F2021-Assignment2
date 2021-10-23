df9a38dd4d3aaca2b12cbfa2a63b9db116f7e513
Sindhu Binil <sbtker@gmail.com>
2021-10-22 23:02:30 -0400

    added address-list page

--- .history/Server/Config/db_20211022205031.js
+++ .history/Server/Config/db_20211022205031.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Secret = exports.HostName = exports.RemoteURI = exports.LocalURI = void 0;
-exports.LocalURI = "mongodb://remotehost/addressbook";
+exports.LocalURI = "mongodb://localhost/addressbook";
 exports.RemoteURI = "mongodb+srv://sindhu:q7tPb16rm0QeYT0O@comp229-f2021.ognyx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 exports.HostName = "remotehost";
 exports.Secret = "someSecret";
--- .history/Server/Config/db_20211022222444.ts
+++ .history/Server/Config/db_20211022222444.ts
@@ -1,4 +1,4 @@
 export const LocalURI = "mongodb://localhost/addressbook";
 export const RemoteURI = "mongodb+srv://sindhu:q7tPb16rm0QeYT0O@comp229-f2021.ognyx.mongodb.net/addressbook?retryWrites=true&w=majority"
-export const HostName = "remotehost";
+export const HostName = "localhost";
 export const Secret = "someSecret";
