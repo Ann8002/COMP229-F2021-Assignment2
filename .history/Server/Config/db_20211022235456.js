7f16fe70abff719518a3fcb84ca1de0fed67740c
Sindhu Binil <sbtker@gmail.com>
2021-10-23 00:01:11 -0400

    minor amendments

---
+++ .history/Server/Config/db_20211022235456.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Secret = exports.HostName = exports.RemoteURI = exports.LocalURI = void 0;
+exports.LocalURI = "mongodb://remotehost/addressbook";
+exports.RemoteURI = process.env.RemoteURI;
+exports.HostName = "remotehost";
+exports.Secret = "someSecret";
+//# sourceMappingURL=db.js.map
---
+++ .history/Server/Config/db_20211022235534.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Secret = exports.HostName = exports.RemoteURI = exports.LocalURI = void 0;
+exports.LocalURI = "mongodb://remotehost/addressbook";
+exports.RemoteURI = "mongodb+srv://sindhu:q7tPb16rm0QeYT0O@comp229-f2021.ognyx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+exports.HostName = "remotehost";
+exports.Secret = "someSecret";
+//# sourceMappingURL=db.js.map
